cecc12288afdfe9ae70e7d833f8fc62143a89dfd
holyub <108190933+Dfonk0@users.noreply.github.com>
2022-09-07 13:26:28 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,18 @@
+	function p0() {
+    var win = window.open(); win.document.body.style.margin = '0'; win.document.body.style.height = '100vh'; var f = win.document.createElement("iframe");  window.focus(); var url = "https://thenewscycle.com/"; if (!url) return; f.style.width = "100%"; f.style.height = "100%"; f.style.border = 'none'; f.style.margin = '0'; win.document.body.appendChild(f); f.src=url
+}
+		function p1() {
+    var win = window.open(); win.document.body.style.margin = '0'; win.document.body.style.height = '100vh'; var f = win.document.createElement("iframe");  window.focus(); var url = "https://hypertabs.cc/"; if (!url) return; f.style.width = "100%"; f.style.height = "100%"; f.style.border = 'none'; f.style.margin = '0'; win.document.body.appendChild(f); f.src=url
+}
+		function p2() {
+    var win = window.open(); win.document.body.style.margin = '0'; win.document.body.style.height = '100vh'; var f = win.document.createElement("iframe");  window.focus(); var url = "https://utopiaunblocker.org/"; if (!url) return; f.style.width = "100%"; f.style.height = "100%"; f.style.border = 'none'; f.style.margin = '0'; win.document.body.appendChild(f); f.src=url
+}
+		function p3() {
+    var win = window.open(); win.document.body.style.margin = '0'; win.document.body.style.height = '100vh'; var f = win.document.createElement("iframe");  window.focus(); var url = "https://browser.rammerhead.org/"; if (!url) return; f.style.width = "100%"; f.style.height = "100%"; f.style.border = 'none'; f.style.margin = '0'; win.document.body.appendChild(f); f.src=url
+}
+		function p4() {
+    var win = window.open(); win.document.body.style.margin = '0'; win.document.body.style.height = '100vh'; var f = win.document.createElement("iframe");  window.focus(); var url = "https://pov.yourdadlike.me/"; if (!url) return; f.style.width = "100%"; f.style.height = "100%"; f.style.border = 'none'; f.style.margin = '0'; win.document.body.appendChild(f); f.src=url
+}
+		function p5() {
+    var win = window.open(); win.document.body.style.margin = '0'; win.document.body.style.height = '100vh'; var f = win.document.createElement("iframe");  window.focus(); var url = "https://pheenie.tech/"; if (!url) return; f.style.width = "100%"; f.style.height = "100%"; f.style.border = 'none'; f.style.margin = '0'; win.document.body.appendChild(f); f.src=url
+}
